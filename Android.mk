tonblack/cm_bootable_recovery

amlogic: add UBIFS support

Android.mk
@@ -18,7 +18,13 @@ LOCAL_SRC_FILES := \
     edifyscripting.c \
     setprop.c \
     default_recovery_ui.c \
-    verifier.c
+    verifier.c \
+    ubi/ubiutils-common.c \
+    ubi/libubi.c
+
+LOCAL_C_INCLUDES += \
+    device/ainol/elf2/mtd-utils/include/ \
+    device/ainol/elf2/mtd-utils/ubi-utils/include
 
 ADDITIONAL_RECOVERY_FILES := $(shell echo $$ADDITIONAL_RECOVERY_FILES)
 LOCAL_SRC_FILES += $(ADDITIONAL_RECOVERY_FILES)

hjanos1/android_bootable_recovery

Conflicts:
	Android.mk
	roots.c
	updater/Android.mk
	updater/install.c

Android.mk
@@ -19,7 +19,13 @@ LOCAL_SRC_FILES := \
     setprop.c \
     default_recovery_ui.c \
     adb_install.c \
-    verifier.c
+    verifier.c \
+    ubi/ubiutils-common.c \
+    ubi/libubi.c
+
+LOCAL_C_INCLUDES += \
+    device/wayteq/w28ref/mtd-utils/include/ \
+    device/wayteq/w28ref/mtd-utils/ubi-utils/include
 
 ADDITIONAL_RECOVERY_FILES := $(shell echo $$ADDITIONAL_RECOVERY_FILES)
 LOCAL_SRC_FILES += $(ADDITIONAL_RECOVERY_FILES)